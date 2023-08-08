06ee5e4f4e3112f033771dbb699432634ed6695c
Hrithik Dhondi <136426393+hrithikdhondi@users.noreply.github.com>
2023-08-08 15:01:58 +0530

    Delete hello_.c

--- hello_.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    if(printf("hello world")){}    
-    return 0; 
-    // switch(printf("hello world")){}    
-    // return 0;
-    // while(!printf("hello world")){}    
-    // return 0;  
-}
