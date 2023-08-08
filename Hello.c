10f7f8a6f1379b8676cbc0377b1198fa89355f61
Hrithik Dhondi <136426393+hrithikdhondi@users.noreply.github.com>
2023-08-08 15:13:15 +0530

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    if(printf("hello world")){}    
+    return 0; 
+    // switch(printf("hello world")){}    
+    // return 0;
+    // while(!printf("hello world")){}    
+    // return 0;  
+}
