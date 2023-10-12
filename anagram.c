6a1ee09d94f3b675ea925f4c70f99dd0f592e0b0
Hrithik Dhondi <136426393+hrithikdhondi@users.noreply.github.com>
2023-10-12 20:07:58 +0530

    anagram.c

--- anagram.c
+++ anagram.c
@@ -2,47 +2,35 @@
 #include<string.h>
 // To check whether the given strings are anagram or not.
 int main(){
-    char str1[20],str2[20],count=0,count1=0,count2=0;
+    char str[20],str1[20];
+    int count=0,count1;
     printf("Enter the first string : ");
-    scanf("%s",str1);
+    scanf("%s",&str);
     printf("Enter the second string : ");
-    scanf("%s",str2);
-    if(strlen(str1)==strlen(str2)){
-        for(int i=0; i<strlen(str1); i++){
-            for(int j=0; j<=strlen(str2); j++){
-                if(str1[i]==str2[j]){
+    scanf("%s",&str1);
+    count1=strlen(str);
+    if(strlen(str)==strlen(str1)){
+        for(int i=0; i<=strlen(str); i++){
+            for(int j=0; j<strlen(str1); j++){
+                if(str[i]==str1[j]){
                     count++;
                     break;
                 }
             }
         }
-        if(strlen(str1)==count){
-            for(int i=0; i<strlen(str1); i++){
-                for(int j=0; j<=strlen(str2); j++){
-                    if(str1[i]==str2[j]){
-                        count1++; 
-                    }
-                }
+        
+            
+            if(count==strlen(str)){
+                printf("The strings are anagram");
             }
-            for(int i=0; i<strlen(str1); i++){
-                for(int j=0; j<=strlen(str2); j++){
-                    if(str1[i]==str2[j]){
-                        count2++;
-                    }
-                }
-            }
-            if(count1==count2){
-                printf("The strings are Anagram");
-            } 
             else{
-                printf("The strings are Anagram");
+                printf("The strings are not anagram");
             }
-        } 
-        else{
-            printf("The strings are Anagram");
-        }
+       
+            
     }
     else{
-        printf("The strings are Anagram");
+        printf("The strings are not anagram");
     }
+    
 }
