0fe75019fe0c263bab026fbd8a711a962087c13e
Hrithik Dhondi <136426393+hrithikdhondi@users.noreply.github.com>
2023-10-12 20:09:03 +0530

    anagram.c

--- anagram.c
+++ anagram.c
@@ -3,7 +3,7 @@
 // To check whether the given strings are anagram or not.
 int main(){
     char str[20],str1[20];
-    int count=0,count1;
+    int count=0,count1,count2=0;
     printf("Enter the first string : ");
     scanf("%s",&str);
     printf("Enter the second string : ");
@@ -18,15 +18,29 @@ int main(){
                 }
             }
         }
-        
+        if(count==strlen(str)){
+            for(int i=0; i<strlen(str); i++){
+                for(int j=0; j<count1; j++){
+                    if(str[i]==str1[j]){
+                        count2++;
+                        for(int k=j; k<count1; k++){
+                            str1[k]=str1[k+1];
+                        }
+                        count1--;  
+                    }       
+                }
+            }
             
-            if(count==strlen(str)){
+            if(count2==strlen(str)){
                 printf("The strings are anagram");
             }
             else{
                 printf("The strings are not anagram");
             }
-       
+        }
+        else{
+            printf("The strings are not anagram");
+        }
             
     }
     else{
